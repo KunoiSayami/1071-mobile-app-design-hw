078f194457153d0d810a0cbbe7d479df31534cd8
KunoiSayami <46131041+KunoiSayami@users.noreply.github.com>
2019-01-01 23:54:42 +0800

    Create OnClickListener * Create README

--- app/src/main/java/com/example/app/hw1071/MainActivity.java
+++ app/src/main/java/com/example/app/hw1071/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.app.hw1071;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    Button bt_confirm;
+    private void init(){
+        bt_confirm = findViewById(R.id.bt_search_food);
+        bt_confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this , DetailActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
